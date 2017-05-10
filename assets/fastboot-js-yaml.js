f0d07d89439924aee419e37a5f3b18bb6dad3db0
Jérémy Buget <jbuget@gmail.com>
2017-05-11 01:01:08 +0200

    Add FastBoot support

---
+++ assets/fastboot-js-yaml.js
@@ -0,0 +1,5 @@
+(function() {
+  if (!self['js-yaml']) {
+    self['js-yaml'] = FastBoot.require('js-yaml');
+  }
+})();
--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = {
-  name: 'ember-js-yaml',
-  included: function included(app) {
-    this._super.included(app);
-    app.import(app.bowerDirectory + '/js-yaml/dist/js-yaml.js');
-    app.import('vendor/js-yaml.js', {
-      exports: {
-        JsYaml: ['default']
-      }
-    });
-  }
+const path = require('path');
+const funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
 
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
+
+module.exports = {
+  name: 'ember-cli-js-yaml',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    let host = this._findHost();
+
+    if (isFastBoot()) {
+      this.importFastBootDependencies(host);
+    } else {
+      this.importBrowserDependencies(host);
+    }
+  },
+
+  treeForVendor(vendorTree) {
+    let trees = [];
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(funnel(path.dirname(require.resolve('js-yaml/dist/js-yaml.js')), {
+      files: ['js-yaml.js'],
+    }));
+
+    if (isFastBoot()) {
+      trees.push(funnel(path.join(__dirname, './assets'), {
+        files: ['fastboot-js-yaml.js']
+      }));
+    }
+
+    return mergeTrees(trees);
+  },
+
+  importFastBootDependencies(app) {
+    let pkg = require(path.join(app.project.root, 'package.json'));
+    let whitelist = pkg.fastbootDependencies;
+
+    if (!whitelist || whitelist && !~whitelist.indexOf('js-yaml')) {
+      throw new Error("[ember-cli-js-yaml] js-yaml is missing from package.json's fastbootDependencies.\nSee: https://github.com/ember-fastboot/ember-cli-fastboot#whitelisting-packages");
+    }
+
+    this.import('vendor/fastboot-js-yaml.js');
+  },
+
+  importBrowserDependencies(app) {
+    this.import('vendor/js-yaml.js');
+  }
 };
